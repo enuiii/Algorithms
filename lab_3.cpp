3dbc42eb0bcf4282297ea1ae679c7b76c3bd2640
enui <115658651+enuiii@users.noreply.github.com>
2023-09-29 09:56:55 +0200

    Add files via upload

---
+++ lab_3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+// Функция для вывода чисел, удовлетворяющих условию
+void printNumbers(int x) {
+	std::vector <int> numbers;
+
+// Вложенные циклы для перебора всех возможных комбинаций значений K, L и M
+	for (int k = 0; k <= log(x) / log(3); k++) {
+		for (int l = 0; l <= log(x) / log(5); l++) {
+			for (int m = 0; m <= log(x) / log(7); m++) {
+				int number = pow(3, k) * pow(5, l) * pow(7, m); // Вычисление числа
+				if (number <= x) {
+					numbers.push_back(number); // Добавление числа в вектор, если оно меньше или равно x
+				}
+			}
+		}
+	}
+
+	// Вывод найденных чисел
+	for (int i = 0; i < numbers.size(); i++) {
+		std::cout << numbers[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main() {
+	int x;
+	std::cout << "Enter a number: ";
+	std::cin >> x;
+	std::cout << "Numbers satisfying the condition: ";
+	printNumbers(x);
+	return 0;
+}
